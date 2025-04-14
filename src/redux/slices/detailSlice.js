71968cab7ae2a01ee0c93c4040e5cab7c5ef6426
Selincevk <selincevk@gmail.com>
2025-04-14 14:21:32 +0300

    düzenleme

--- src/redux/slices/detailSlice.js
+++ src/redux/slices/detailSlice.js
@@ -1,40 +1,43 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { getDetail } from "../actions";
 const initialState = {
-    flightId : null,
-    isLoading: false,
-    error: null,
-    info:null, // uçuş bilgileri
-    route:[] // güzergah
-
-}
+  flightId: null,
+  isLoading: false,
+  error: null,
+  info: null, // uçuş bilgileri
+  route: [], // güzergah
+};
 const detailSlice = createSlice({
-name : "detail",
-initialState,
-reducers: { open: (state, action) => {
-    state.flightId = action.payload;
-  },
+  name: "detail",
+  initialState,
+  reducers: {
+    open: (state, action) => {
+      state.flightId = action.payload;
+    },
 
-  close: (state) => {
-    state.flightId = null;
-  },},
-extraReducers:(builder) => {
+    close: (state) => {
+      state.flightId = null;
+      state.info = null;
+      state.route = [];
+    },
+  },
+  extraReducers: (builder) => {
     builder.addCase(getDetail.pending, (state) => {
-        state.isLoading = true;
-      });
-  
-      builder.addCase(getDetail.rejected, (state, { error }) => {
-        state.isLoading = false;
-        state.error = error.message;
-      });
-  
-      builder.addCase(getDetail.fulfilled, (state, { payload }) => {
-        state.isLoading = false;
-        state.error = null;
-        state.info = payload;
-        state.route = payload.trail;
-      });
-}
-})
+      state.isLoading = true;
+    });
+
+    builder.addCase(getDetail.rejected, (state, { error }) => {
+      state.isLoading = false;
+      state.error = error.message;
+    });
+
+    builder.addCase(getDetail.fulfilled, (state, { payload }) => {
+      state.isLoading = false;
+      state.error = null;
+      state.info = payload;
+      state.route = payload.trail;
+    });
+  },
+});
 export const { open, close } = detailSlice.actions;
-export default detailSlice.reducer
+export default detailSlice.reducer;
